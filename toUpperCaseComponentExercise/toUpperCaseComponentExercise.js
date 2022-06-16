38e2510d15e975c6e52ce9704274102ea8202594
Sunil Singh <sinnghsunil@gmail.com>
2022-06-16 19:03:27 +0530

    All changes done

--- toUpperCaseComponentExercise/toUpperCaseComponentExercise.js
+++ toUpperCaseComponentExercise/toUpperCaseComponentExercise.js
@@ -3,6 +3,7 @@ import { LightningElement } from 'lwc';
 export default class ToUpperCaseComponentExercise extends LightningElement {
 
     output;
+    
     handleChange(event){
         this.output = event.target.value.toUpperCase();
     }
