898d9f5e2b45ebcc556e5d8c1512147c59644a6d
Sunil Singh <sinnghsunil@gmail.com>
2022-06-16 07:55:32 +0530

    importing module from same folder of component

---
+++ moduleImportExport/moduleImportExport.js
@@ -0,0 +1,25 @@
+import { LightningElement } from 'lwc';
+
+
+// importing the module from the util.js . No need to use .js extension while importing
+import {sum , firstName} from './util';
+
+// importing the module from the common js file . after from use c/name of the js file with out .js extention
+import {oddOrEven} from 'c/commonJsCodeAreaForAllComponent'
+
+
+export default class ModuleImportExport extends LightningElement {
+
+    
+handleClick(event){
+    // the function from util.js can only be used inside a function not inside a class directly
+    // module imported from common code area
+    console.log(sum(10,20));
+
+
+// the function from util.js can only be used inside a function not inside a class directly
+    console.log(oddOrEven(99));
+    alert(firstName);
+}
+
+}
---
+++ moduleImportExport/util.js
@@ -0,0 +1,14 @@
+// Custom javascript file created in the same folder of component
+
+
+export function sum(x,y){
+    return x+y;
+}
+
+
+export const firstName = "Sunil Singh";
+
+// we can export all at once 
+
+// export {firstName , sum}; 
+// we will not be using export keyword with function and variable if we are export all at once
