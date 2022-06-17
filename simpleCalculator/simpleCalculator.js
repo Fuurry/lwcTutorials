15717bc0a62e81e027a2306c661ab7db1f2fc317
Sunil Singh <sinnghsunil@gmail.com>
2022-06-17 07:16:43 +0530

    Code using Template querySelector added

--- simpleCalculator/simpleCalculator.js
+++ simpleCalculator/simpleCalculator.js
@@ -5,18 +5,20 @@ export default class SimpleCalculator extends LightningElement {
     input2;
     output;
 
+
+    // fecthing input field using template querySelector , we don't require on change event anymore
     handleOnchange(event){
-        // using event.target.name to check the origin of the event 
-        if(event.target.name == 'input1'){
-            // setting value of input1 variable as passed in input field of lwc
-            this.input1=+event.target.value;
+        // // using event.target.name to check the origin of the event 
+        // if(event.target.name == 'input1'){
+        //     // setting value of input1 variable as passed in input field of lwc
+        //     this.input1=+event.target.value;
             
-        }
-        else if (event.target.name=='input2'){
-            // setting value of input2 variable as passed in input field of lwc
-            this.input2 = +event.target.value;
+        // }
+        // else if (event.target.name=='input2'){
+        //     // setting value of input2 variable as passed in input field of lwc
+        //     this.input2 = +event.target.value;
             
-        }
+        // }
 
         
     }
@@ -25,6 +27,11 @@ export default class SimpleCalculator extends LightningElement {
 
     handleClick(event){
         // using event.target.name to check the origin of the event 
+        // fetching value using template querySelector
+        this.input1 = +this.template.querySelector("lightning-input[data-id='input1']").value;
+        this.input2 = +this.template.querySelector("lightning-input[data-id='input2']").value;
+
+        
         
         if(event.target.name=='add'){
             this.output = this.input1+this.input2;
